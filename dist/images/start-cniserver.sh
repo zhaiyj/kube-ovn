2962b35c9cff7921504e3a9c6cf94191557f3941
phonzia <phonzia@gmail.com>
2023-11-22 14:15:00 +0800

    kill old process when cniserver start to advoid binding error (#53)
    * kill old process when cniserver start to advoid binding error
    
    * merge changes from community version

--- dist/images/start-cniserver.sh
+++ dist/images/start-cniserver.sh
@@ -4,9 +4,23 @@ set -euo pipefail
 CNI_SOCK=/run/openvswitch/kube-ovn-daemon.sock
 OVS_SOCK=/run/openvswitch/db.sock
 ENABLE_SSL=${ENABLE_SSL:-false}
+SYSCTL_NF_CONNTRACK_TCP_BE_LIBERAL=${SYSCTL_NF_CONNTRACK_TCP_BE_LIBERAL:-1}
+SYSCTL_IPV4_NEIGH_DEFAULT_GC_THRESH=${SYSCTL_IPV4_NEIGH_DEFAULT_GC_THRESH:-"1024 2048 4096"}
+
+# usage: set_sysctl key value
+function set_sysctl {
+    echo "setting sysctl variable \"$1\" to \"$2\""
+    procfs_path="/proc/sys/$(echo "$1" | tr . /)"
+    if [ -f "$procfs_path" ]; then
+      sysctl -w "$1=$2"
+    else
+      echo "path \"$procfs_path\" does not exist, skip"
+    fi
+}
 
 function quit {
-    rm -rf CNI_CONF
+    rm -rf $CNI_SOCK
+    ps -ef|grep kube-ovn-daemon|grep -v grep|awk '{print $2}'|xargs kill -9
     exit 0
 }
 trap quit EXIT
@@ -14,24 +28,35 @@ trap quit EXIT
 if [[ -e "$CNI_SOCK" ]]
 then
     echo "previous socket exists, remove and continue"
-	rm ${CNI_SOCK}
+    rm ${CNI_SOCK}
 fi
 
-while true
-do
-  sleep 1
-  if [[ -e "$OVS_SOCK" ]]
-  then
-    break
-  else
+while true; do
+    if [[ -e "$OVS_SOCK" ]]; then
+        /usr/share/openvswitch/scripts/ovs-ctl status && break
+    fi
     echo "waiting for ovs ready"
-  fi
+    sleep 1
 done
+
+# update links to point to the iptables binaries
+iptables -V
 
 # If nftables not exist do not exit
 set +e
 iptables -P FORWARD ACCEPT
 iptables-nft -P FORWARD ACCEPT
 set -e
+
+gc_thresh1=$(echo "$SYSCTL_IPV4_NEIGH_DEFAULT_GC_THRESH" | awk '{print $1}')
+gc_thresh2=$(echo "$SYSCTL_IPV4_NEIGH_DEFAULT_GC_THRESH" | awk '{print $2}')
+gc_thresh3=$(echo "$SYSCTL_IPV4_NEIGH_DEFAULT_GC_THRESH" | awk '{print $3}')
+set_sysctl net.ipv4.neigh.default.gc_thresh1 $gc_thresh1
+set_sysctl net.ipv4.neigh.default.gc_thresh2 $gc_thresh2
+set_sysctl net.ipv4.neigh.default.gc_thresh3 $gc_thresh3
+set_sysctl net.netfilter.nf_conntrack_tcp_be_liberal $SYSCTL_NF_CONNTRACK_TCP_BE_LIBERAL
+
+echo "killing old process"
+ps -ef|grep kube-ovn-daemon|grep -v grep|awk '{print $2}'|xargs kill -9
 
 ./kube-ovn-daemon --ovs-socket=${OVS_SOCK} --bind-socket=${CNI_SOCK} "$@"
